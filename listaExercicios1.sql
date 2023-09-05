c4b5edf6b7e35a347014d9cab989fa9776a4ce8d
Renan Salvador de Santana <renan.salvador@eaportal.org>
2023-09-05 20:55:37 -0300

    Decima terceira questao resolvida

--- listaExercicios1.sql
+++ listaExercicios1.sql
@@ -72,3 +72,9 @@ SELECT alunos.nome, matriculas.curso
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id;
 }
+
+Questão 13 {
+SELECT autores.nome, livros.titulo
+FROM autores
+LEFT JOIN livros ON autores.id = livros.autor_id;
+}
